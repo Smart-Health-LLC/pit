674a137cae7d841087ed313f94c641774d939a42
volard <kirillzhilenkov@gmail.com>
2024-05-03 12:13:16 +0800

    :wrench: fix(android): initialize napier to have logs

--- composeApp/src/androidMain/kotlin/com/smarthealth/pit/KmpApplication.kt
+++ composeApp/src/androidMain/kotlin/com/smarthealth/pit/KmpApplication.kt
@@ -2,6 +2,8 @@ package com.smarthealth.pit
 
 import android.app.Application
 import di.KoinInit
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import org.koin.android.ext.koin.androidContext
 
 // For Android we need to start koin a bit differently since most of the time Android requires
@@ -10,6 +12,7 @@ import org.koin.android.ext.koin.androidContext
 class KmpApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Napier.base(DebugAntilog())
         KoinInit().init {
             androidContext(androidContext = this@KmpApplication)
 
