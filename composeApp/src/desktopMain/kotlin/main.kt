f3985c7662f9cf7a2f04f2e6c13f3c879d7897f8
volard <kirillzhilenkov@gmail.com>
2024-05-04 21:52:13 +0800

    :lipstick: feat: add desktop app icon

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,14 +1,20 @@
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 import di.KoinInit
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.imageResource
 import org.koin.core.Koin
+import pit.composeapp.generated.resources.Res
+import pit.composeapp.generated.resources.logo_round
 
 
 lateinit var koin: Koin
 
+@OptIn(ExperimentalResourceApi::class)
 fun main() {
     koin = KoinInit().init()
     koin.loadModules(
@@ -18,6 +24,7 @@ fun main() {
 
     return application {
         Window(
+            icon = BitmapPainter(image = imageResource(Res.drawable.logo_round)),
             onCloseRequest = ::exitApplication,
             title = "pit",
             state = rememberWindowState(
