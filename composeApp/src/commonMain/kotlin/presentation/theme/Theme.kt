bc19613e5bdc361ee489b982b4750fb06c84c221
volard <kirillzhilenkov@gmail.com>
2024-05-19 03:32:29 +0800

    :lipstick: refactor: remove custom theme colors

--- composeApp/src/commonMain/kotlin/presentation/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/presentation/theme/Theme.kt
@@ -1,49 +1,18 @@
 package presentation.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
-
-private val darkColorPalette = darkColorScheme(
-    primary = PrimaryColor,
-    primaryContainer = PrimaryVariantColor,
-    secondary = AccentColor,
-)
-
-private val lightColorPalette = lightColorScheme(
-    primary = PrimaryColor,
-    primaryContainer = PrimaryVariantColor,
-    secondary = AccentColor,
-    background = Color.White,
-    surfaceVariant = Color.White,
-    surface = lightSurface,
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
-
-
+/**
+ * This wrapper is useful if some style adjustments will be required in the future
+ */
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        darkColorPalette
-    } else {
-        lightColorPalette
-    }
-
     MaterialTheme(
-        colorScheme = colors,
         content = content
     )
 }
