1eac0350c6e79b4047c7ccd9884ff8b55619f707
volard <kirillzhilenkov@gmail.com>
2024-05-18 23:24:06 +0800

    :recycle: refactor: remove voyager navigator provider

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -6,21 +6,18 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import cafe.adriel.lyricist.LocalStrings
 import cafe.adriel.lyricist.ProvideStrings
+import cafe.adriel.voyager.navigator.CurrentScreen
 import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.transitions.SlideTransition
-import domain.repository.SettingsRepository
 import i18n.lyricist
 import org.koin.compose.KoinContext
 import org.koin.compose.koinInject
 import presentation.theme.AppTheme
 import presentation.ui.main.*
 import presentation.ui.onboarding.OnboardingScreen
-import presentation.wtf.ProvideAppNavigator
 
 @Composable
 fun App(
     mainViewModel: MainViewModel = koinInject(),
-    settingsRepository: SettingsRepository = koinInject()
 ) {
     val isDarkTheme = when (mainViewModel.appTheme.collectAsState().value) {
         1 -> true
@@ -48,21 +45,14 @@ fun App(
                                 } else {
                                     OnboardingScreen()
                                 },
-                                content = { navigator ->
-                                    ProvideAppNavigator(
-                                        navigator = navigator,
-                                        content = { SlideTransition(navigator = navigator) },
-                                    )
-                                },
+                                content = { CurrentScreen() },
                             )
                         }
                     }
-                    // todo I completely lost in this magic "when" block
-                    else -> {}
+
+                    is OnBoardingState.Loading -> {}
                 }
             }
-
-
         }
     }
 }
